a3579e418f332ba00488b06f52d20d10df1e7283
Polichronucci <nick@discloud.eu>
2017-07-31 14:45:36 +0300

    Remove merge f1240d6e568fb31d2ce6c72b185d4211fc5fa739 leftovers
    The email contents for the body variable are not needed. They
    should have been removed by the merge.

--- Routes/post/postmathima.php
+++ Routes/post/postmathima.php
@@ -167,11 +167,7 @@ Email: ................... {$fields['edu_quest_applicant_email']}
 Τίτλος Μεταπτυχιακού: .... {$fields['edu_quest_graduate_title']}\n";
 
         	$stmt->execute();
-		$content = '<table><tr><td>ΙΔΡΥΜΑ</td><td>ΣΧΟΛΗ</td><td>ΜΑΘΗΜΑ</td><td>ΤΕΧΝΟΛΟΓΙΑ</td><td>URL</td><tr>';
-		$content .= "<tr><td>".$dget["idrima"];
-		$content .= "</td><td>";
-		$content .= $dget["sxolh"];
-		$content .= "</td>";
+
         	$lastid = $storage->lastInsertId();
 		if($data['eidikotita'] == 'didaktiko' || $data['eidikotita'] == 'fititis' || $data['eidikotita'] == 'ergastiriako'){
 			$g1 = 'INSERT INTO datamathima ( "id", "mathima", "ellak", "ellakurl" ) VALUES ( :lastid, :mathima, :ellak, :ellakurl)';
@@ -190,24 +186,14 @@ Email: ................... {$fields['edu_quest_applicant_email']}
 					}else{
 						$stmt1->bindValue(':mathima', $dget["ellak"][$i]["mathima"]);
 						$contentellakm = $dget["ellak"][$i]["mathima"];
-						$contentellakme = "<td>";
-						$contentellakme .= $dget["ellak"][$i]["mathima"];
-+						$contentellakme .= "</td>";
 					}
 					if($dget["ellak"][$i]["tech"] != ''){
 						$stmt1->bindValue(':ellak', $dget["ellak"][$i]["tech"]);
 						$contentellakt = $dget["ellak"][$i]["tech"];
-						$contentellakte = "<td>";
-						$contentellakte .= $dget["ellak"][$i]["tech"];
-						$contentellakte .= "</td>";
 					}
 					elseif($dget["ellak"][$i]["url"] != ''){
 						$stmt1->bindValue(':ellakurl', $dget["ellak"][$i]["url"]);
 						$contentellaku = $dget["ellak"][$i]["url"];
-						$contentellakue = "<td>";
-						$contentellakue .= $dget["ellak"][$i]["url"];
-						$contentellakue .= "</td>";
-						$contentellakue .= "</tr>";
 					}
 				if($i == $ii){
 					$fields['edu_quest_course']= $contentellakm;
@@ -232,15 +218,6 @@ Email: ................... {$fields['edu_quest_applicant_email']}
 
 					$exec1 .= " 2>&1";
 					exec($exec1, $output1, $return_var1);
-
-					$content .= $contentellakme;
- 					$content .= $contentellakte;
- 					$content .= $contentellakue;
-					$content .= '<td></td><td></td>';
- 					$contentellakme='';
- 					$contentellakte='';
- 					$contentellakue='';
-
 					$contentellakm='';
 					$contentellakt='';
 					$contentellaku='';
